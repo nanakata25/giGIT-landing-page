439122eab7038d0edeb20b579a58831ba7658a72
unknown <fahmihadiwijaya82@gmail.com>
2026-03-18 01:39:07 +0700

    nambah kode fitur sikit

--- Main text editor.c
+++ Main text editor.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 
 void newFile(){
+    printf("\n=== New File ===\n");
 
+    char text[100][100];
 }
 
 void openFile(){
+    FILE *fp;
+    char namafile[100][100];
+    char baca;
 
+    printf("\n=== Open File ===\n");
+    printf("Masukkan Nama File:");
+    scanf("%s", namafile);
+
+    fp = fopen(namafile,"r+");
+
+    if (fp == NULL){
+        printf("File Tidak Ada!!\n");
+        return;
+    }
 }
 
 void saveFile(){
